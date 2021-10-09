28da3e8089fce1ecd22eb5071b72efab3fcb7162
Sahil Saha <sahilsaha7773@gmail.com>
2021-10-09 17:27:21 +0530

    Added gitignore

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./routers/userRouter");
