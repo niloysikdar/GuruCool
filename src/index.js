d60fb1d88414b2fbbb02ac74c1d5d5c2e50d7c0a
Niloy Sikdar <niloysikdar30@gmail.com>
2021-10-09 14:18:52 +0530

    Added Basic styling and Buttons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <BrowserRouter>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import styles from './login.module.scss';
+
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={styles.main}>
+      <div className={styles.box}>
+        <div className={styles.imagewrapper}></div>
+        <div className={styles.form}>
+          <div className={styles.loginselector}>
+            <button
+              className={isLogin && styles.selected}
+              onClick={() => setIsLogin(true)}
+            >
+              Login
+            </button>
+            <button
+              className={!isLogin && styles.selected}
+              onClick={() => setIsLogin(false)}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
